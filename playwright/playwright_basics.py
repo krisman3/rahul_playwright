db619654cc9980c1ee4ac4688f80f6af7836d0fc
krisman3 <kriskokz@gmail.com>
2026-06-08 16:48:32 +0300

    first playwright func

---
+++ playwright/playwright_basics.py
@@ -0,0 +1,9 @@
+
+
+def test_playwright_basics(playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://rahulshettyacademy.com")
+
+    
