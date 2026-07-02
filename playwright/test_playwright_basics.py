affead46ec837f3ea833efd3dafbdc12dbf4e29b
kristiyan <kristiyan.iliev@dxc.com>
2026-07-02 16:01:34 +0300

    incorrect creds

--- playwright/test_playwright_basics.py
+++ playwright/test_playwright_basics.py
@@ -1,5 +1,6 @@
 import time
 from playwright.sync_api import Page
+from playwright.sync_api import expect
 
 
 # def test_playwright_basics(playwright):
@@ -18,6 +19,21 @@ def test_core_locators(page: Page):
     page.get_by_label("Username:").fill("rahulshettyacademy")
     page.get_by_label("Password:").fill("learning")
     page.get_by_role("combobox").select_option("teach")
+    page.locator("#terms").check()
     page.get_by_role("link", name="terms and conditions").click()
     page.get_by_role("button", name="Sign In").click()
     time.sleep(5)
+
+
+def test_incorrect_credentials(playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://rahulshettyacademy.com/loginpagePractise")
+    page.get_by_label("Username:").fill("rahulshettyacademy")
+    page.get_by_label("Password:").fill("learningfdfd")
+    page.get_by_role("combobox").select_option("teach")
+    page.locator("#terms").check()
+    page.get_by_role("link", name="terms and conditions").click()
+    page.get_by_role("button", name="Sign In").click()
+    expect(page.get_by_text("Incorrect username/password.")).to_be_visible()
