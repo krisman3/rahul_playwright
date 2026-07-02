399b1f2f105cf84ef764f4aabdac9ca1acf2d729
kristiyan <kristiyan.iliev@dxc.com>
2026-07-02 15:38:37 +0300

    test core loc

--- playwright/test_playwright_basics.py
+++ playwright/test_playwright_basics.py
@@ -1,9 +1,23 @@
+import time
+from playwright.sync_api import Page
 
 
-def test_playwright_basics(playwright):
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
+# def test_playwright_basics(playwright):
+    # browser = playwright.chromium.launch(headless=False)
+    # context = browser.new_context()
+    # page = context.new_page()
+    # page.goto("https://rahulshettyacademy.com")
+
+
+def test_playwright_shortcut(page: Page):
     page.goto("https://rahulshettyacademy.com")
 
-    
+
+def test_core_locators(page: Page):
+    page.goto("https://rahulshettyacademy.com/loginpagePractise")
+    page.get_by_label("Username:").fill("rahulshettyacademy")
+    page.get_by_label("Password:").fill("learning")
+    page.get_by_role("combobox").select_option("teach")
+    page.get_by_role("link", name="terms and conditions").click()
+    page.get_by_role("button", name="Sign In").click()
+    time.sleep(5)
