845cb6d30cabc0fb7ddfe1c861f253ff7d26ff40
kristiyan <kristiyan.iliev@dxc.com>
2026-07-09 16:05:27 +0300

    expanded with iframe example

--- playwright/test_more_validations.py
+++ playwright/test_more_validations.py
@@ -4,10 +4,24 @@ from playwright.sync_api import Page, expect
 
 
 def test_placeholder(page: Page):
+    # Hide/display and placeholder
     page.goto("https://rahulshettyacademy.com/AutomationPractice/")
     expect(page.get_by_placeholder("Hide/Show Example")).to_be_visible()
     page.get_by_role("button", name="Hide").click()
     expect(page.get_by_placeholder("Hide/Show Example")).to_be_hidden()
+
+    #Alert Boxes
+    page.on("dialog", lambda dialog:dialog.accept())
+    page.get_by_role("button", name="Confirm").click()
+
+
+    # iFrame Example
+    page_frame = page.frame_locator("#courses-iframe")
+    page_frame.get_by_role("link", name="All Access plan").click()
+    expect(page_frame.locator("body")).to_contain_text("Happy Subscibers")
+
+
+
 
 
 def test_email(page: Page):
