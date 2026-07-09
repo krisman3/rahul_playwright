be4c219702d6aaadf91d8d6514d34f81aef13bf6
kristiyan <kristiyan.iliev@dxc.com>
2026-07-09 16:43:00 +0300

    added a test to check prices in a table

--- playwright/test_more_validations.py
+++ playwright/test_more_validations.py
@@ -1,6 +1,7 @@
 import time
 
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page, expect, Playwright
+from pytest_playwright.pytest_playwright import playwright, context
 
 
 def test_placeholder(page: Page):
@@ -21,11 +22,25 @@ def test_placeholder(page: Page):
     expect(page_frame.locator("body")).to_contain_text("Happy Subscibers")
 
 
-
-
-
 def test_email(page: Page):
     page.goto("https://rahulshettyacademy.com/client/#/auth/login")
     page.locator("#userEmail").fill("test123")
     page.locator("#userPassword").fill("pass123")
     time.sleep(3)
+
+
+def test_check_price_in_table(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False, args=["--start-maximized"])
+    context = browser.new_context(no_viewport=True)
+    page = context.new_page()
+    page.goto("https://rahulshettyacademy.com/seleniumpractice/#/offers")
+
+    row = page.locator("table tbody tr").filter(has_text="Tomato")  # can be changed with "item_name" and can be passed as argument
+    headers = page.locator("table thead th")
+    header_texts = headers.all_inner_texts()
+    price_col_index = header_texts.index("Price")
+
+    price_cell = row.locator("td").nth(price_col_index)
+
+    assert price_cell.inner_text() == "37"
+
