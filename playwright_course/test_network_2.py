e81cedbf73dc016c07882972a842f58788dbfc2b
kristiyan <kristiyan.iliev@dxc.com>
2026-07-13 16:39:16 +0300

    network interceptions

--- playwright_course/test_network_2.py
+++ playwright_course/test_network_2.py
@@ -1,11 +1,12 @@
-from playwright.sync_api import Page, Route
+from playwright.sync_api import Page
 
 fake_payload_no_orders = {"data": [], "message": "No Orders"}
 
 
-def intercept_response(route: Route):
-    route.fulfill(
-        json=fake_payload_no_orders
+def intercept_response(route):
+    route.continue_(
+        route.continue_(url="https://rahulshettyacademy.com/client/dashboard/order-details/6711d910ae2afd4c0b9f67c6")
+
     )
 
 
@@ -14,12 +15,11 @@ def test_network(page: Page):
     # Login
     page.goto("https://rahulshettyacademy.com/client")
 
-    page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*", intercept_response)
+    page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=*", intercept_response)
     page.get_by_placeholder("email@example.com").fill("rahulshetty@gmail.com")
     page.get_by_placeholder("enter your passsword").fill("Iamking@000")
     page.get_by_role("button", name="Login").click()
 
     # Orders History page -> Order is present
     page.get_by_role("button", name="ORDERS").click()
-    order_text = page.locator(".mt-4").text_content()
-    print(order_text)
+
