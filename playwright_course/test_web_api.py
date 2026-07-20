9685a5b3289ce0ae570b7f777c163aeb9e67d3e9
kristiyan <kristiyan.iliev@dxc.com>
2026-07-20 13:35:57 +0300

    fixed a mistake

--- playwright_course/test_web_api.py
+++ playwright_course/test_web_api.py
@@ -4,18 +4,18 @@ import pytest
 from playwright.sync_api import Playwright, expect
 from utils.api_base import APIUtils
 
+# Json file -> util -> Access into test
+with open('data/credentials.json', 'r') as f:
+    test_data = json.load(f)
+    print(test_data)
+    user_credentials_list = test_data['user_credentials']
 
-@pytest.mark.parametrize
+@pytest.mark.parametrize(user_credentials_list)
 def test_e2e_web_api(playwright: Playwright):
     browser = playwright.chromium.launch()  # headless=True, args=["--start-maximized"]
     context = browser.new_context()  # no_viewport=True
     page = context.new_page()
 
-    # Json file -> util -> Access into test
-    with open('data/credentials.json', 'r') as f:
-        test_data = json.load(f)
-        print(test_data)
-        user_credentials_list = test_data['user_credentials']
     # Create order -> Order ID
     api_utils = APIUtils()
     order_id = api_utils.create_order(playwright)
