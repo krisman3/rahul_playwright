ae103dbd31a4e1423a5a057e0405987733f84124
kristiyan <kristiyan.iliev@dxc.com>
2026-07-20 12:50:56 +0300

    changed auth

--- playwright_course/test_network_1.py
+++ playwright_course/test_network_1.py
@@ -15,10 +15,9 @@ def test_network(page: Page):
     page.goto("https://rahulshettyacademy.com/client")
 
     page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*", intercept_response)
-    page.get_by_placeholder("email@example.com").fill("rahulshetty@gmail.com")
-    page.get_by_placeholder("enter your passsword").fill("Iamking@000")
+    page.get_by_placeholder("email@example.com").fill("email_kristiyan@email.com")
+    page.get_by_placeholder("enter your passsword").fill("Pass1234")
     page.get_by_role("button", name="Login").click()
-
     # Orders History page -> Order is present
     page.get_by_role("button", name="ORDERS").click()
     order_text = page.locator(".mt-4").text_content()
